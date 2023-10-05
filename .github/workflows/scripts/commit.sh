68f5b9efd2aef1d08633a893f697dc3fb6876600
rgallor <riccardo.gallo@secomind.com>
2023-10-05 15:49:28 +0200

    WIP
    Signed-off-by: rgallor <riccardo.gallo@secomind.com>

---
+++ .github/workflows/scripts/commit.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Copyright 2023 SECO Mind Srl
+# SPDX-License-Identifier: Apache-2.0
+
+set -exEuo pipefail
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add -v rust/edgehog-device-forwarder-proto
+git diff --staged --quiet || git commit -s -m "chore(rust): update generated code"
+git push
